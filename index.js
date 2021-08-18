2c25456e977aa73996c01283f39953cbbf848a4f
Yazan Baker <yazanbaker94@hotmail.com>
2021-08-18 13:41:44 +0300

    lab9 server

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const { db } = require('./src/models');
 
 db.sync().then(() => {
 // Start the web server
-require('./src/server').start(process.env.PORT||4000);
+require('./src/server').start(process.env.PORT);
 });
